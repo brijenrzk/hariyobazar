ae2711f1a7cc693e877deff174b59c30de733239
Brijen Rajak <brijenrajak@gmail.com>
2020-10-18 11:26:08 +0545

    adde files folder for static files

--- hariyobazar/settings.py
+++ hariyobazar/settings.py
@@ -126,4 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
